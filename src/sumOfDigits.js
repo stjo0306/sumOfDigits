7252d386ff4cce39e39b81b8b3e4bd3128a35852
John Steckelberg <jsteckelberg@nteglobal.com>
2022-09-06 13:56:50 -0500

    update to use math syntax shortcut

--- src/sumOfDigits.js
+++ src/sumOfDigits.js
@@ -2,7 +2,7 @@ const sumOfDigits = (number) => {
   let num = number;
   let sum = 0;
   while (num > 0) {
-    sum = sum + (num % 10);
+    sum += (num % 10);
     num = Math.floor(num / 10);
   }
   return sum;
